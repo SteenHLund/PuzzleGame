66b6970bfefd42d30dc4f0c00ba8b0f7dedfda54
SteenHLund <steen.h.lund@gmail.com>
2016-01-12 12:23:47 +0100

    Update Game.java
    made the movement work! Also added a victory screen

--- Game.java
+++ Game.java
@@ -6,6 +6,8 @@ import java.awt.PointerInfo;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -14,20 +16,20 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Game extends Info implements ActionListener, MouseListener {
+public class Game extends Info implements ActionListener, MouseListener, KeyListener {
 	public static GamePanel gamePanel;
 	public static Timer timer;
-	public static JButton quit = new JButton(), start = new JButton(), useless = new JButton();
+	public static JButton quit = new JButton(), boardSelect = new JButton(), useless = new JButton();
 	public static JLabel gameTime = new JLabel(), title = new JLabel();
 	
 	public Game(int gameBoardType) {
 		boardType = gameBoardType;
 		if(boardType == 3)
-			boardSize = new Dimension(500, 300);
+			boardSize = new Dimension(275, 330);
 		else if(boardType == 4)
-			boardSize = new Dimension(550, 370);
+			boardSize = new Dimension(335, 390);
 		else if(boardType == 5)
-			boardSize = new Dimension(600, 440);
+			boardSize = new Dimension(395, 450);
 		
 		gameFrame = new JFrame();
 		gamePanel = new GamePanel();
@@ -70,8 +72,10 @@ public class Game extends Info implements ActionListener, MouseListener {
 		boolean isNeighbor = false;
 		int zero = zeroPos();
 		
-		if(pos - boardType == zero || pos + boardType == zero || (pos - 1 == zero && pos % boardType != 1) || (pos + 1 == zero && pos % boardType != 0))
+		if(pos - boardType == zero || pos + boardType == zero || (pos - 1 == zero && pos % boardType != 0) || (pos + 1 == zero && pos % boardType != boardType - 1))
 			isNeighbor = true;
+		else
+			System.out.println(pos % boardType);
 		
 		return isNeighbor;
 	}
@@ -83,24 +87,18 @@ public class Game extends Info implements ActionListener, MouseListener {
 		title.setSize(titleSize);
 		title.setText("Sliding Puzzle Ultra!");
 		title.setVisible(true);
-		addComponent(title, boardSize.width - title.getWidth() - 10, title.getHeight() + 30, titleSize);
+		addComponent(title, 65 + (boardType - 3) * 30, 20, titleSize);
 		
 		gameTime.setSize(titleSize);
 		gameTime.setText("Time: " + 0.0);
 		gameTime.setVisible(true);
-		addComponent(gameTime, boardSize.width - gameTime.getWidth() + 24, quit.getHeight() + 205, titleSize);
+		addComponent(gameTime, 45, gameFrame.getHeight() - 75, titleSize);
 		
 		quit.setSize(size);
 		quit.setText("Quit");
 		quit.setVisible(true);
 		quit.addActionListener(this);
-		addComponent(quit, boardSize.width - quit.getWidth() - 48, quit.getHeight() + 90, size);
-		
-		start.setSize(size);
-		start.setText("Start");
-		start.setVisible(true);
-		start.addActionListener(this);
-		addComponent(start, boardSize.width - start.getWidth() - 48, start.getHeight() + 30, size);
+		addComponent(quit, gameFrame.getWidth() - 140, gameFrame.getHeight() - 90, size);
 		
 		useless.setVisible(false);
 		addComponent(useless, 0, 0, size);
@@ -114,16 +112,24 @@ public class Game extends Info implements ActionListener, MouseListener {
 	public void actionPerformed(ActionEvent e) {
 		Object src = e.getSource();
 		
-		if(src == start) {
-			started = true;
-			timer.start();
-		} else if(src == quit)
+		boolean stillGood = true;
+		for(int i = 0; i < numbers.size() - 1; i++)
+			if(numbers.get(i).getValue() != i + 1)
+				stillGood = false;
+		
+		if(stillGood) {
+			gameOver = true;
+			gameFrame.repaint();
+		}
+		
+		if(src == quit)
 			System.exit(0);
 		
 		if(gameOver == false  && started) {
 			time += TIMER_SPEED;
 			gameTime.setText("Time: " + (time / 1000) + "." + (time / 100 % 10));
-		}
+		} else if (gameOver)
+			timer.stop();
 	}
 
 	public void mouseClicked(MouseEvent e) {
@@ -135,31 +141,34 @@ public class Game extends Info implements ActionListener, MouseListener {
 	}
 
 	public void mouseReleased(MouseEvent e) {
-		PointerInfo pointLoc = MouseInfo.getPointerInfo();
-		Point mouseLoc = pointLoc.getLocation();
-		System.out.println(mouseLoc.getX() - 471 + " " + numbers.get(24).getPos().x);
+		started = true;
+		if (!timer.isRunning())
+			timer.start();
+		
+		Point mouseLoc = gameFrame.getMousePosition();
 		boolean clickedAlready = false;
 		
-		for(int i = 0; i < numbers.size(); i++) {
-			if (clickedAlready == false) {
-				if (mouseLoc.getX() - 471 < numbers.get(i).blockInfo().x + numbers.get(i).blockInfo().getWidth()
-						&& mouseLoc.getX() - 471 > numbers.get(i).blockInfo().x) {
-					if (mouseLoc.getY() - 307 < numbers.get(i).blockInfo().y + numbers.get(i).blockInfo().getHeight()
-							&& mouseLoc.getY() - 307 > numbers.get(i).blockInfo().y) {
-						if (nextToZero(i)) {
-							System.out.println("Block " + numbers.get(i).getValue() + " was clicked!");
-							int tempZeroSpace = zeroPos();
-							Block tempZeroBlock = numbers.get(tempZeroSpace);
-							Block tempSwappingBlock = numbers.get(i);
-							Point temp = tempSwappingBlock.getPos();
-							tempSwappingBlock.setPos(tempZeroBlock.getPos());
-							
-							tempZeroBlock.setPos(temp);
-							
-							numbers.set(tempZeroSpace, numbers.get(i));
-							numbers.set(i, tempZeroBlock);
-							
-							clickedAlready = true;
+		if (!gameOver) {
+			for (int i = 0; i < numbers.size(); i++) {
+				if (clickedAlready == false) {
+					if (mouseLoc.getX() < numbers.get(i).blockInfo().x + numbers.get(i).blockInfo().getWidth()
+							&& mouseLoc.getX() > numbers.get(i).blockInfo().x) {
+						if (mouseLoc.getY() - 25 < numbers.get(i).blockInfo().y + numbers.get(i).blockInfo().getHeight()
+								&& mouseLoc.getY() - 25 > numbers.get(i).blockInfo().y) {
+							if (nextToZero(i)) {
+								int tempZeroSpace = zeroPos();
+								Block tempZeroBlock = numbers.get(tempZeroSpace);
+								Block tempSwappingBlock = numbers.get(i);
+								Point temp = tempSwappingBlock.getPos();
+								tempSwappingBlock.setPos(tempZeroBlock.getPos());
+
+								tempZeroBlock.setPos(temp);
+
+								numbers.set(tempZeroSpace, numbers.get(i));
+								numbers.set(i, tempZeroBlock);
+
+								clickedAlready = true;
+							}
 						}
 					}
 				}
@@ -173,6 +182,18 @@ public class Game extends Info implements ActionListener, MouseListener {
 	}
 
 	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
 		
 	}
 }
