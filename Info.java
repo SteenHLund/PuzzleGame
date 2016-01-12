572e1abc2fa625c4d8bf6d4c3894f8ac6ec5f05e
SteenHLund <steen.h.lund@gmail.com>
2016-01-12 15:32:25 +0100

    updated Info.java
    Added names of the high score files

--- Info.java
+++ Info.java
@@ -8,7 +8,7 @@ public class Info extends JPanel {
 	final int TIMER_SPEED = 100;
 	final int TIMER_DELAY = 0;
 	
-	public final Dimension blockSize = new Dimension(50, 50);
+	public final Dimension blockSize = new Dimension(57, 57);
 
 	public static int time = 0, boardType;
 
@@ -19,4 +19,6 @@ public class Info extends JPanel {
 	public static boolean gameOver = false, started = false;
 	
 	public static ArrayList<Block> numbers = new ArrayList<Block>();
+	
+	public static String high3 = "3x3High", high4 = "4x4High", high5 = "5x5High";
 }
