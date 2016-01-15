9cf7be7931bde19aecb9e70f4f1cd5e9382f9ea5
SteenHLund <steen.h.lund@gmail.com>
2016-01-15 16:40:35 +0100

    Update Info.java
    Add in the readers and writers for files as well as change the file name

--- Info.java
+++ Info.java
@@ -1,16 +1,25 @@
 import java.awt.Dimension;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Info extends JPanel {
+	private static final long serialVersionUID = 1L;
+	
 	final int TIMER_SPEED = 100;
 	final int TIMER_DELAY = 0;
 	
 	public final Dimension blockSize = new Dimension(57, 57);
 
 	public static int time = 0, boardType;
+	
+	public BufferedReader input;
+	public FileWriter output;
+	public PrintWriter printOut;
 
 	public static JFrame gameFrame;
 	
@@ -19,6 +28,7 @@ public class Info extends JPanel {
 	public static boolean gameOver = false, started = false;
 	
 	public static ArrayList<Block> numbers = new ArrayList<Block>();
+	public static ArrayList<String> highscores = new ArrayList<String>();
 	
-	public static String high3 = "3x3High", high4 = "4x4High", high5 = "5x5High";
+	public static String fileName = "";
 }
