a226929acd8fcc3a9e58679e70e52c0c304afd4c
SteenHLund <steen.h.lund@gmail.com>
2016-01-17 20:52:16 +0100

    Update RunGame.java
    Add comments

--- RunGame.java
+++ RunGame.java
@@ -1,4 +1,19 @@
+//Created by Steen, Wikkee, and Anina
+//Project for Software 1
+//Number slider game
 
+//Features included are:
+
+//Music with the ability to stop the music again
+//A working slider game
+//Both keyboard and mouse controls
+//An undo button
+//A timer that records the game time
+//3 unique high score boards for the different game sizes
+//3 Different game sizes. 3x3, 4x4, and 5x5
+//The game gets harder as the time goes on and the blocks start changing colors
+
+//Simply runs the game by creating the start menu.
 public class RunGame {
 	public static void main(String[] args) {
 		StartMenu newGame = new StartMenu();
