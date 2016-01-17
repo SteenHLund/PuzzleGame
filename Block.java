4cb538361929865126a36a4fdeb83d5f8a77d9d6
SteenHLund <steen.h.lund@gmail.com>
2016-01-17 20:49:44 +0100

    Update Block.java
    Add in comments

--- Block.java
+++ Block.java
@@ -2,6 +2,7 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
 
+//A simple class that creates the block object.  The blocks are the squares in the grid for the game
 public class Block {
 	private Dimension blockSize;
 	private int number;
@@ -13,22 +14,27 @@ public class Block {
 		pos = place;
 	}
 	
+	//Gets the size of the block
 	public Dimension getSize() {
 		return blockSize;
 	}
 	
+	//Gets the number on the block
 	public int getValue() {
 		return number;
 	}
 	
+	//Gets its x and y chordinates
 	public Point getPos() {
 		return pos;
 	}
 	
+	//Sets the new x and y chordinates
 	public void setPos(Point newPos) {
 		pos = newPos;
 	}
 	
+	//Gives the square that is formed from the block
 	public Rectangle blockInfo() {
 		return new Rectangle(pos.x, pos.y, blockSize.width, blockSize.height);
 	}
