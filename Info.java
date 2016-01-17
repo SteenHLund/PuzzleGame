23e91e6ecd0a4ea2e894a9010751eb16f3d90608
SteenHLund <steen.h.lund@gmail.com>
2016-01-17 20:51:39 +0100

    Update Info.java
    Add comments

--- Info.java
+++ Info.java
@@ -3,10 +3,10 @@ import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+//A class used entirely for the purpose of sharing variables between the classes
 public class Info extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
@@ -15,7 +15,7 @@ public class Info extends JPanel {
 	
 	public final Dimension blockSize = new Dimension(57, 57);
 
-	public static int time = 0, boardType;
+	public static int time = 0, boardType, previous = 8, numMoves = 0;
 	
 	public BufferedReader input;
 	public FileWriter output;
@@ -30,5 +30,7 @@ public class Info extends JPanel {
 	public static ArrayList<Block> numbers = new ArrayList<Block>();
 	public static ArrayList<String> highscores = new ArrayList<String>();
 	
-	public static String fileName = "";
+	//The song below is not owned by us and is used simply as a proof of concept that it works.  It would never
+	//be included in anything not used for educational purposes in a classroom
+	public static String fileName = "", musicName = "Was Not Was - Walk The Dinosaur.mp3";
 }
